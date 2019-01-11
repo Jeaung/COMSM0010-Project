3c49d45c1ec45605ed15b24c6fffe67e2580bf2d
Jeaung <brucejeaung@gmail.com>
2019-01-11 17:56:53 +0000

    1. add primary key to match table; 2. add aurora serverless cluster

--- server/befree/init.js
+++ server/befree/init.js
@@ -9,11 +9,14 @@ exports.initHandler = async (event, context, callback) => {
             host: config.get('db_config.host'),
             port: config.get('db_config.port'),
             user: config.get('db_config.user'),
-            password: config.get('db_config.password'),
-            database: config.get('db_config.database')
+            password: config.get('db_config.password')
         });
 
-        var result = await connection.query('create table if not exists matches (id bigint unsigned, home_team varchar(64), away_team varchar(64), home_score tinyint unsigned, away_score tinyint unsigned, status varchar(16), date_time bigint unsigned) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+        var dbRes = await connection.query('create database ' + config.get('db_config.database') + ' if not exists');
+
+        console.log('create database result', dbRes);
+
+        var result = await connection.query('create table if not exists ' + config.get('db_config.database') + '.matches (id bigint unsigned, home_team varchar(64), away_team varchar(64), home_score tinyint unsigned, away_score tinyint unsigned, status varchar(16), date_time bigint unsigned, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
 
         console.log('create table result', result);
 
